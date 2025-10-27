38aecc004296ed939f73a1ac21c2c7a24b587ea9
EgorCharntsev <charncev200447@gmail.com>
2025-10-27 21:53:00 +0300

    update build.gradle.kts and add auto build ci pipeline

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,19 +1,82 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.4" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
-group = "ru.meetpoint"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "ru.meetpoint"
+    version = "0.0.1-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "jacoco")
 
-tasks.test {
-    useJUnitPlatform()
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(17)
+        }
+    }
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-security")
+        implementation("org.springframework.boot:spring-boot-starter-web")
+
+        compileOnly("org.projectlombok:lombok")
+        testCompileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.boot:spring-boot-testcontainers")
+        testImplementation("org.springframework.security:spring-security-test")
+        testImplementation("org.testcontainers:junit-jupiter")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+        testImplementation("org.mockito:mockito-core")
+        testImplementation("org.mockito:mockito-junit-jupiter")
+
+        testImplementation("org.wiremock:wiremock-standalone:3.12.1")
+
+        compileOnly("org.jetbrains:annotations:26.0.1")
+    }
+
+    configure<JacocoPluginExtension> {
+        toolVersion = "0.8.12"
+    }
+
+    tasks.named<JacocoReport>("jacocoTestReport") {
+        dependsOn(tasks.test)
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+        classDirectories.setFrom(
+            fileTree("${layout.buildDirectory.get()}/classes/java/main") {
+                exclude("**/generated/**")
+            }
+        )
+        sourceDirectories.setFrom(files("src/main/java"))
+        executionData.setFrom(
+            fileTree(layout.buildDirectory).include("jacoco/test.exec")
+        )
+    }
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    tasks.named<Test>("test") {
+        useJUnitPlatform()
+        finalizedBy(tasks.named<JacocoReport>("jacocoTestReport"))
+    }
 }
